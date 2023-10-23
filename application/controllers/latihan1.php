eba0c98481fde73a4ff9031840bd5e28789e0bf5
19221125 <19221125@bsi.ac.id>
2023-10-23 15:40:31 +0700

    menambahkan template tugas 4

---
+++ application/controllers/latihan1.php
@@ -0,0 +1,15 @@
+<?php 
+class Latihan1 extends CI_Controller 
+{ 
+    public function index() 
+    {
+     
+        echo "<h1>Perkenalkan</h1>"; 
+        echo "Nama  : Zidan Alif Fadhillah <br>"; 
+        echo "NIM   : 1921125<br>"; 
+        echo "HP    : 082297272475<br>"; 
+        echo "Hobi  : bulutangkis<br>"; 
+        echo "alamat: bekasi,Serang Baru<br>"; 
+        echo "Umur  : 19"; 
+    } 
+} 
---
+++ application/views/v_about.php
@@ -0,0 +1,21 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Zidan</li>
+ <li>Nama Belakang : Alif F</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Serang Baru</li>
+ </ul>
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Bekasi</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ </ul>
+</section>
